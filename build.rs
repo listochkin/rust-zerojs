2d8656472e516d78432cbf978ff6be1de73eafa6
Andrey Listochkin <listochkin@outlook.com>
2015-01-10 22:26:47 +0200

    Cargo build script for duktape

---
+++ build.rs
@@ -0,0 +1,22 @@
+#![allow(unstable)]
+use std::io::Command;
+use std::os;
+
+fn main() {
+    let out_dir = os::getenv("OUT_DIR").unwrap();
+
+    // 1. duktape.o
+    Command::new("gcc").args(&["deps/duktape/src/duktape.c", "-c", "-o"])
+                       .arg(format!("{}/duktape.o", out_dir))
+                       .status().unwrap();
+    // 2. dt.o
+    Command::new("gcc").args(&["-I", "deps/duktape/src", "src/dt/dt.c", "-c", "-o"])
+                       .arg(format!("{}/dt.o", out_dir))
+                       .status().unwrap();
+    // 3. libdt.a
+    Command::new("ar").args(&["crus", "libdt.a", "dt.o", "duktape.o"])
+                      .cwd(&Path::new(&out_dir))
+                      .status().unwrap();
+    // 4. configure rustc
+    println!("cargo:rustc-flags=-L {} -l dt:static", out_dir);
+}
